d08e85da66e9aba1dbdd126db21ccf8dbfd01d98
Yunus <seytnazarov.yunus@gmail.com>
2021-12-13 12:57:02 +0300

    Closure II Updates

--- js/script.js
+++ js/script.js
@@ -1,45 +1,35 @@
+let callFunction = 0;
+
 const generationNumArr = function () {
   const arr = [];
+
   return function randomNumber() {
+    callFunction += 1;
     const newNum = Math.floor(Math.random() * 100 + 1);
     if (arr.length === 100) {
-      return;
+      return arr;
     }
 
     if (arr.includes(newNum)) {
       return randomNumber();
     }
     arr.push(newNum);
-    return newNum;
+    return randomNumber();
   };
 };
 
 const result = generationNumArr();
 
 console.log(result());
-console.log(result());
-console.log(result());
-console.log(result());
-console.log(result());
-console.log(result());
-console.log(result());
-console.log(result());
-console.log(result());
-console.log(result());
-console.log(result());
-console.log(result());
-console.log(result());
-console.log(result());
-console.log(result());
-console.log(result());
-console.log(result());
+console.log(callFunction);
 
 const getRandomNumber = () => Math.floor(Math.random() * 100 + 1);
-
+let callFunctionSecond = 0;
 const generateId = function () {
   const arr = [];
 
   function generateNumber() {
+    callFunctionSecond += 1;
     const randomNum = getRandomNumber();
     if (arr.length === 100) {
       return arr;
@@ -67,6 +57,7 @@ foo = foo.sort((a, b) => {
 });
 
 console.log(foo);
+console.log(callFunctionSecond);
 
 // Сделайте функцию, каждый вызов который будет генерировать случайные
 // числа от 1 до 100, но так, чтобы они не повторялись, пока не будут
